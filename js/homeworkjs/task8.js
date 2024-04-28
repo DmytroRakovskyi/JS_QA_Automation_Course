1190e939269f1263d9c6c152ab5358934935281e
DmytroRakovskyi <dmytriy.rakovskiy@gmail.com>
2024-04-28 18:27:20 +0200

    upd: task 8

--- js/homeworkjs/task8.js
+++ js/homeworkjs/task8.js
@@ -9,8 +9,8 @@ const getUniqueElements = (arr) => {
   }
   return noDuplicatesArr;
 };
-getUniqueElements([2, 7, 7, 5, 4, 2, 7]);
-console.log(getUniqueElements([2, 7, 7, 5, 4, 2, 7]));
+const res1 = getUniqueElements([2,1,1,2,2,2,7, 5, 4, 2, 7,77,27]);
+console.log(res1);
 
 /* Task 2
 Створити функцію sum яка отримує масив чисел і повертає суму елементів масиву. Наприклад sum([1, 1, 4, 3]) має повернути 9. */
@@ -24,46 +24,12 @@ const sum = (arr) => {
   return total;
 };
 
-sum([1, 1, 4, 3]);
-console.log(sum([1, 1, 4, 3]));
-/* Створити функцію partialReverse яка отримує масив і повертає масив у якому всі елементи крім першого і останнього мають зворотній порядок.
-Створити функцію glueArrays яка приймає у якості параметрів 2 масиви і повертає масив або null, приклади:  glueArrays([1,2,3,4], [4,5,8,6]) має повернути [1,2,3,4,5,8,6], glueArrays([1,2,3,3], [3,3,8,6]) має повернути [1,2,3,3,3,8,6], glueArrays([1,2,3,4], [5,2,8,6]) має повернути null.
-Написати функцію яка змінює порядок літер в рядку на зворотній. Використовувати методи reverse, toReversed заборонено.
-Написати функцію яка повертає true якщо всі елементи масива не мають дублікатів. У іншому випадку функція має повертати false. Використання Set заборонено.
-Написати функцію removeNegative яка прибирає з масиву чисел негативні елементи. Наприклад removeNegative([-2,4,3,-10,0,3]) має повернути [4,3,0,3]. Використання filter заборонено.
-https://www.codewars.com/kata/5679aa472b8f57fb8c000047/javascript
+const sum1 = sum([1, 1, 4, 3,993]);
+console.log(sum1);
 
-Створити функцію partialReverse яка отримує масив і повертає масив у якому всі елементи крім першого і останнього мають зворотній порядок. */
+// Task 3
+/*  Створити функцію partialReverse яка отримує масив і повертає масив у якому всі елементи крім першого і останнього мають зворотній порядок. */
 console.log('Task 3');
-
-/* const partialReverse = (arr) => {
-let newArr = [];
-for (let i = 0; i < arr.length; i++) {
-    if (i !== 0 && i !== arr.length - 1)
-    newArr.push(arr[i]);
-}
-newArr.reverse();
-newArr.unshift(arr[0]);
-newArr.push(arr[arr.length - 1]);
-return newArr
-}
-partialReverse(['first', 'second', 'third', 'four'])
-console.log(partialReverse(['first', 'second', 'third', 'four'])) */
-
-/* const partialReverse = (arr) => {
-let newArr = [];
-for (let el of arr) {
-    newArr.push(el);
-}
-newArr.splice(0,1);
-newArr.splice(-1,1);
-newArr.reverse();
-newArr.unshift(arr[0]);
-newArr.push(arr[arr.length - 1]);
-return newArr
-}
-const result = partialReverse(['first', 'second', 'third', 'four']);
-console.log(partialReverse(result)); */
 
 const partialReverse = (arr) => {
   const newArr = arr.slice();
@@ -74,11 +40,13 @@ const partialReverse = (arr) => {
   newArr.push(arr.at(-1));
   return newArr;
 };
-const result = partialReverse(['first', 'second', 'third', 'four']);
+const result = partialReverse(['first', 'second', 'third', 'four', 'five', 'six']);
 console.log(result);
 
+// Task 4
 /* Створити функцію glueArrays яка приймає у якості параметрів 2 масиви і повертає масив або null, приклади:  glueArrays([1,2,3,4], [4,5,8,6]) має повернути [1,2,3,4,5,8,6], glueArrays([1,2,3,3], [3,3,8,6]) має повернути [1,2,3,3,3,8,6], glueArrays([1,2,3,4], [5,2,8,6]) має повернути null. */
 console.log('Task 4');
+
 const glueArrays = (arr, arr2) => {
   if (arr.at(-1) === arr2.at(0)) {
     arr.pop();
@@ -92,8 +60,10 @@ const glueArrays = (arr, arr2) => {
 const result4 = glueArrays([1, 2, 3, 4], [4, 5, 8, 6]);
 console.log(result4);
 
+// Task 5
 // Написати функцію яка змінює порядок літер в рядку на зворотній. Використовувати методи reverse, toReversed заборонено.
 console.log('Task 5');
+
 function reverseI(arr) {
   const arr2 = [];
   for (const el of arr) {
@@ -104,3 +74,42 @@ function reverseI(arr) {
 }
 const result5 = reverseI(['a', 'b', 'c', 'k', 'j']);
 console.log(result5);
+
+// Task 6
+/*Написати функцію яка повертає true якщо всі елементи масива не мають дублікатів. У іншому випадку функція має повертати false. Використання Set заборонено. */
+console.log('Task 6'); 
+
+
+const isDuplicate = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      if (i !== j && arr[i] === arr[j]) {
+        return false; 
+      }
+    }
+  }
+  return true;
+}
+
+
+const resUlt = isDuplicate([1,2,3,4,5,6]);
+console.log(resUlt);
+
+// Task 7
+/* Написати функцію removeNegative яка прибирає з масиву чисел негативні елементи. Наприклад removeNegative([-2,4,3,-10,0,3]) має повернути [4,3,0,3]. Використання filter заборонено. */
+console.log('Task 7'); 
+
+const removeNegative = (arr) => {
+let arrNew = [];
+for (let el of arr) {
+  if (el>=0)
+  arrNew.push(el)
+}
+
+return arrNew;
+}
+const resUlt1 = removeNegative([-1,4,3,0,3,-9999,2]);
+console.log(resUlt1);
+
+// Task 8
+// Codewars solution https://www.codewars.com/kata/reviews/5679c8ba5da43f03ad0000a5/groups/662e776a57e10e00018c7181
